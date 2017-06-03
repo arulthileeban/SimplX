4e5cf64be92c791e9954abce8d512cab92ea4ceb
shrikanth7698 <shrikanth7698@gmail.com>
2017-06-03 14:44:34 +0530

    Included new fonts and splash screen

---
+++ app/src/main/java/com/example/shrika/simplx/splash.java
@@ -0,0 +1,50 @@
+package com.example.shrika.simplx;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        AssetManager am = getApplicationContext().getAssets();
+        Typeface custom = Typeface.createFromAsset(am,
+                String.format(Locale.US, "fonts/%s", "Freeroad Light.ttf"));
+        final TextView title = (TextView) findViewById(R.id.TitleView);
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.my_animation);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                title.startAnimation(animation);
+            }
+        }, 500);
+
+        final Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent main =new Intent(splash.this,MainActivity.class);
+                startActivity(main);
+                splash.this.overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        }, 2500);
+    }
+}
