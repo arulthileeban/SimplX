8b915b9e78a96800bc421050677f0d50a58e638a
arulthileeban <arulthileeban023@gmail.com>
2017-06-03 14:16:02 +0530

    Flask Application added

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import request
+import os
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+app.config['ALLOWED_EXTENSIONS'] = set(['txt','wav', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def nlp_process(data):
+#Insert NLP Function here
+	code=""
+	return code
+	
+def ip_process(data):
+#Insert IP Function here 
+	code=""
+	return code
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/voice', methods=['GET', 'POST'])
+def voice():
+    data = request.args.get('text')
+    s_code= nlp_process(data)
+    return s_code
+
+
+@app.route('/picture',methods=['GET','POST'])
+def picture():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename=file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        s_code=ip_process(filename)
+        return s_code
+
+@app.route('/')
+def index():
+    return "Hey"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',debug=True)
